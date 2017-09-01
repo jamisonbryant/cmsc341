88d32907dde839a8803222619a4817e8835ec91e
Jamison Bryant <jbryant93@umbc.edu>
2017-09-01 09:37:40 -0400

    Stop rm from bitching about missing files

---
+++ proj0/src/proj0.cpp
@@ -0,0 +1,33 @@
+// File: proj0.cpp
+//
+// CMSC 341 Fall 2017 Project 0
+//
+// Buggy program to be tested with gdb
+
+#include <iostream>
+#include <cmath>
+#include "proj0.h"
+
+using namespace std ;
+
+int main() {
+
+   int previous, current, size ;
+
+   int *ptr ;
+
+   previous = 0 ;
+
+   for (int i=1 ; i < 35 ; i++) {
+
+      current = chaos(i) ;
+      size = current - previous ;
+
+      ptr = new int[size] ;  // allocate array
+
+      previous = current ;
+      delete [] ptr ;
+   }
+
+   return 0 ;
+}
